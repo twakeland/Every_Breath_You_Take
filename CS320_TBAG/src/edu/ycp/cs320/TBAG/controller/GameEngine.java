40a681ab0f9dd151e008f82642f817d91780683d
mvoithofer <mvoithofer@ycp.edu>
2025-04-03 20:22:04 -0400

    VERY Basic NPC Implementation
    Added NPC class, updated room class and game engine.
    TempNPC added to Hallway (Room 2) for testing.

--- CS320_TBAG/src/edu/ycp/cs320/TBAG/controller/GameEngine.java
+++ CS320_TBAG/src/edu/ycp/cs320/TBAG/controller/GameEngine.java
@@ -2,25 +2,29 @@ package edu.ycp.cs320.TBAG.controller;
 
 import edu.ycp.cs320.TBAG.model.Room;
 import edu.ycp.cs320.TBAG.model.Item;
-import edu.ycp.cs320.TBAG.model.ItemWeapons;
-import edu.ycp.cs320.TBAG.model.ItemConsumables;
 import edu.ycp.cs320.TBAG.model.Player;
+import edu.ycp.cs320.TBAG.model.NPC;
 
 public class GameEngine {
 	private Room start, hallway, lab, basement;
-    private ItemWeapons axe;
-    private ItemConsumables healthKit, oxygenTank;
-    private Player user;
+	private Item axe, healthKit, oxygenTank;
+	private Player user;
+	private NPC tempNPC;
+	
 	
 	public String setData() {
-		start = new Room(1, "You're in starting area", "Welcome to the starting area");
+		start = new Room(1, "You're in deez nuts starting area", "Welcome to the starting area");
 		hallway = new Room(2, "You're in a long hallway", "The hallway is long and dark. You can see a light in the distance. There is an axe leaning on the wall");
 		lab = new Room(3, "You're in the lab", "The lab is filled with tons of scientific equipment you don't recognize. There is a medkit on the desk");
 		basement = new Room(4, "You're in the basement", "The basement is cold and damp, you shouldn't be here. You can see an oxygen tank hidden in the dark");
 		
-		axe = new ItemWeapons(5, 12, "A worn axe used to break down wooden barricades", 10);
-        healthKit = new ItemConsumables(1, 20, "A packet filled with single-use health stims", 50);
-        oxygenTank = new ItemConsumables(Integer.MAX_VALUE, 35, "A sizeable oxygen tank. Great for longer trips underwater", 0);
+		//Temp addNPC to hallway
+		tempNPC = new NPC(5, 2, false, "A mysterious stranger stands in the corner, his face masked by shadows.");
+		hallway.addNPC(tempNPC);
+		
+		axe = new Item(5, 12, "A worn axe used to break down wooden barricades");
+		healthKit = new Item(5, 20, "A packet filled with single-use health stims");
+		oxygenTank = new Item(0, 35, "A sizeable oxygen tank. Great for longer trips underwater");		
 		
 		start.makeConnection("west", 2);
 		hallway.makeConnection("north", 3);
@@ -49,7 +53,18 @@ public class GameEngine {
 				}
 				else {
 					currentRoom.setHasVisited(true);
-					return currentRoom.getLongDesc();
+					//Good lord please ignore how shitty this convo system is so far-->
+					System.out.println(currentRoom.containsNPCS());
+					if(currentRoom.containsNPCS()){
+						String tempString = currentRoom.getLongDesc();
+						tempString += "\ntest" + currentRoom.NPCS.get(0).getTempConvo();
+						
+						return tempString;
+					}
+					
+					System.out.println("No NPCS detected, initial descr overridden");
+					return currentRoom.getLongDesc() + "beans";
+					
 				}
 			}
 			else {
